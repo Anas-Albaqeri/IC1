a39c9d737f6e4d012a25bddcab5305bd77d79e8a
Anas-Albaqeri <127996785+Anas-Albaqeri@users.noreply.github.com>
2023-05-11 10:54:23 +0300

    Add files via upload

---
+++ IC1/BE/policies/change_status.php
@@ -0,0 +1,33 @@
+<?php
+        include '../../functions/functions.php';
+        if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $change_status = $_POST['change_status'];
+            $policy_number = $_POST['policy_number'];
+            $comment = $_POST['comment'];
+            
+            if ($change_status == "Cancel"){
+                $result =  cancel($policy_number);
+                if ($result == true ){
+                    echo "<script>alert('Policy Canceld successfully!')</script>";
+                }
+                else{
+                    echo "<script>alert('Policy cancelation failed!')</script>";
+                }
+            }
+            else{
+              $result = flag($policy_number);
+                if ($result == true){
+                    echo "<script>alert('Policy Flagged successfully!')</script>";
+                    header("Location: ../../policies/pendingpolicies.php");
+                }
+                else{
+                    echo "<script>alert('Policy Flag failed!')</script>";
+                    header("Location: ../../policies/pendingpolicies.php");
+
+                }
+            }
+            }
+    header("Location: allpolicies.php");
+
+
+?> 
